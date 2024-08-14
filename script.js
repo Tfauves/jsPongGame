c317a9a2cc54b96f2d26e9ea6eabf8bc187f539e
Travis J <tfauvelle@gmail.com>
2024-08-14 09:44:34 -0400

    ball movement and paddle1 and paddle2 collision logic

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const startGameText = document.getElementById("startText");
 const paddle1 = document.getElementById("paddle1");
+const paddle2 = document.getElementById("paddle2");
+const ball = document.getElementById("ball");
+
 const paddleAcceleration = 1;
 const maxPaddleSpeed = 5;
+const paddleDeceleration = 1;
+const gameHeight = 400;
+const gameWidth = 600;
 
 let gameRunning = false;
 let keysPressed = {};
+
 let paddle1Speed = 0;
 let paddle1Y = 150;
+
+let paddle2Speed = 0;
+let paddle2Y = 150;
+
+let ballX = 290;
+let ballSpeedX = 2;
+
+let ballY = 190;
+let ballSpeedY = 2;
 
 document.addEventListener("keydown", startGame);
 document.addEventListener("keydown", handleKeyDown);
@@ -23,6 +39,8 @@ function startGame() {
 function gameLoop() {
   if (gameRunning) {
     updatePaddle1();
+    updatePaddle2();
+    moveBall();
     setTimeout(gameLoop, 8);
   }
 }
@@ -40,8 +58,77 @@ function updatePaddle1() {
     paddle1Speed = Math.max(paddle1Speed - paddleAcceleration, -maxPaddleSpeed);
   } else if (keysPressed["s"]) {
     paddle1Speed = Math.min(paddle1Speed + paddleAcceleration, maxPaddleSpeed);
+  } else {
+    if (paddle1Speed > 0) {
+      paddle1Speed = Math.max(paddle1Speed - paddleDeceleration, 0);
+    } else if (paddle1Speed < 0) {
+      paddle1Speed = Math.min(paddle1Speed + paddleDeceleration, 0);
+    }
   }
   paddle1Y += paddle1Speed;
 
+  if (paddle1Y < 0) {
+    paddle1Y = 0;
+  }
+
+  if (paddle1Y > gameHeight - paddle1.clientHeight) {
+    paddle1Y = gameHeight - paddle1.clientHeight;
+  }
+
   paddle1.style.top = paddle1Y + "px";
+}
+
+function updatePaddle2() {
+  if (keysPressed["ArrowUp"]) {
+    paddle2Speed = Math.max(paddle2Speed - paddleAcceleration, -maxPaddleSpeed);
+  } else if (keysPressed["ArrowDown"]) {
+    paddle2Speed = Math.min(paddle2Speed + paddleAcceleration, maxPaddleSpeed);
+  } else {
+    if (paddle2Speed > 0) {
+      paddle2Speed = Math.max(paddle2Speed - paddleDeceleration, 0);
+    } else if (paddle2Speed < 0) {
+      paddle2Speed = Math.min(paddle2Speed + paddleDeceleration, 0);
+    }
+  }
+  paddle2Y += paddle2Speed;
+
+  if (paddle2Y < 0) {
+    paddle2Y = 0;
+  }
+
+  if (paddle2Y > gameHeight - paddle2.clientHeight) {
+    paddle2Y = gameHeight - paddle2.clientHeight;
+  }
+
+  paddle2.style.top = paddle2Y + "px";
+}
+
+function moveBall() {
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+
+  if (ballY >= gameHeight - ball.clientHeight || ballY <= 0) {
+    ballSpeedY = -ballSpeedY;
+  }
+
+  // paddle 1 collision
+  if (
+    ballX <= paddle1.clientWidth &&
+    ballY >= paddle1Y &&
+    ballY <= paddle1Y + paddle1.clientHeight
+  ) {
+    ballSpeedX = -ballSpeedX;
+  }
+
+  // paddle 2 collision
+  if (
+    ballX >= gameWidth - paddle2.clientWidth - ball.clientWidth &&
+    ballY >= paddle2Y &&
+    ballY <= paddle2Y + paddle2.clientHeight
+  ) {
+    ballSpeedX = -ballSpeedX;
+  }
+
+  ball.style.left = ballX + "px";
+  ball.style.top = ballY + "px";
 }
